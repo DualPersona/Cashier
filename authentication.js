f005aa108b8804417e386767f9586d838f210300
DualPersona <arni2003@gmail.com>
2025-01-09 22:34:12 +0100

    added nfc reader permission reinitializer for one more last time if the first permission dialogue was declined

--- authentication.js
+++ authentication.js
@@ -30,9 +30,19 @@ function submitForm(event) {
     Authenticate(formData)
 }
 
-function loginNFC() {
+
+function nfcScannerInitialize(isFirstRun) {
     if ("NDEFReader" in window){
-        const nfcReader = new NDEFReader()
+        const modalFooter = document.getElementById("add-manually-footer")
+        modalFooter.style.display = "flex"
+        modalFooter.style.flexDirection = "row-reverse"
+        modalFooter.style.justifyContent = "space-between"
+        if (isFirstRun) { //ha nem az első futtatása a funkciónak akkor ne generáljon le még egyet
+            modalFooter.innerHTML += '<button id="nfc-status">NFC státusz</button>'
+        }
+        
+
+        let nfcReader = new NDEFReader() //maradjon let hogy lehessen szükség szerint másik példányt hozzárendelni és újrafuttatni a funkciót és a példány metódusait
         
         nfcReader.onreading = function(event) {
             const nfcdata = new TextDecoder('utf-8').decode(event.message.records[0].data)
@@ -48,14 +58,31 @@ function loginNFC() {
         }
 
         nfcReader.scan()
-        .then( () => {
-            console.log("Nfc szkennelés");
+        .then(() => {
+            document.getElementById("nfc-status").className = "nfc-ok"
+        })
+        .catch((err) => {
+            document.getElementById("nfc-status").className = "nfc-invalid"
+            document.getElementById("nfc-status").onclick = () => {
+                if (err.name === "NotAllowedError") {
+                    alert("Hiba: A felhasználó nem engedélyezte az NFC használatát!")
+                    nfcReader = new NDEFReader() //hozzárendelünk egy másik új példányt a változóhoz és felülírjuk az előző referenciáját/hivatkozását
+                    if (isFirstRun) {
+                        nfcScannerInitialize(false) //újra futtatjuk a funkciót az új NDEFReader példányunkkal és beadjuk a funkciónak hogy ez nem az első futtatása
+                    }
+                }
+                else if (err.name === "NotSupportedError") {
+                    alert("Hiba: A készüléknek nincs érvényes NFC olvasója vagy az NFC olvasónak nem sikerült felvennie a kapcsolatot a kiszolgálóval!")
+                }
+                else {
+                    alert(`Hiba: ${err}`)
+                }
+            }
         })
     }
-    else{
-        alert("Ez a böngésző nem támogatja az NFC belépési módot")
-    }
 }
+
+nfcScannerInitialize(true)
 
 function checkAuth() {
     fetch('login.php')
