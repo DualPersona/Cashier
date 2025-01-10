e61e2403e671ff2931a5e86cc73cae6588feeb72
Arnold <62279470+DualPersona@users.noreply.github.com>
2025-01-10 10:38:50 +0100

    cartExport() ID:0 filter

--- backend.js
+++ backend.js
@@ -75,7 +75,9 @@ function renderCategories() {
 function cartExport(){
     const adattomb = []
     for (let tabla_sor of document.getElementById("item-tbody").rows){
+if(tabla_sor.cells[1].dataset.value !== 0){
         adattomb.push({Darabszam: tabla_sor.cells[0].firstElementChild.value, termekID: tabla_sor.cells[1].dataset.value})
+}
     }
 
     fetch('export.php', {method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(adattomb)})
